5bd65c0fda7b5242a854f56384531f05f69f1c8b
gm <146473511+Pooh1206@users.noreply.github.com>
2023-10-21 15:10:48 +0000

    Update Navbar.vue

--- nuxt-app/components/Navbar.vue
+++ nuxt-app/components/Navbar.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 
-// 1. 通过props来实现标题和右侧文字的设置
 const props = defineProps<{
   title?: string
   rightText?: string
@@ -10,12 +9,12 @@ const props = defineProps<{
 const emit = defineEmits<{
   (e: 'click-right'): void
 }>()
-// 2. 使用emit函数来触发自定义事件（点击右侧文字按钮）
+
 const onClickRight = () => {
   emit('click-right')
 }
 const router = useRouter()
-// 3. 回退，了解 history.state 信息，监听箭头的点击事件按条件进行跳转
+
 const onClickLeft = () => {
   if (props.back) return props.back()
   if (history.state?.back) {
@@ -27,7 +26,7 @@ const onClickLeft = () => {
 </script>
 
 <template>
-  <!-- 固定定位，左侧箭头，标题，右侧文字 -->
+
   <van-nav-bar
     fixed
     left-arrow
