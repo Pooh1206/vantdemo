1fa413e438737e175a542efbfcb9247cafda9bab
gm <146473511+Pooh1206@users.noreply.github.com>
2023-10-21 10:18:22 +0000

    origin

---
+++ liffts/components/Navbar/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <van-nav-bar
+      fixed
+      left-arrow
+      @click-left="onClickLeft"
+      :title="title"
+      :right-text="rightText"
+      @click-right="onClickRight"
+    ></van-nav-bar>
+</template>
+
+<style lang="scss" scoped>
+:deep() {
+  .van-nav-bar {
+    &__arrow {
+      color: var(--cp-text1);
+      font-size: 18px;
+    }
+    &__title {
+      font-size: 15px;
+    }
+  }
+}
+</style>
---
+++ liffts/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <slot />
+<Navbar />
+</div>
+</template>
+
+<style lang="scss">
+:deep() {
+  .van-nav-bar {
+    &__arrow {
+      color: var(--cp-text1);
+      font-size: 18px;
+    }
+    &__title {
+      font-size: 15px;
+    }
+  }
+}
+</style>
+  
