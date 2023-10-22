313807cd6d2f1de69f0a8c23a6775fdfed78886c
gm <146473511+Pooh1206@users.noreply.github.com>
2023-10-22 07:57:08 +0000

    Update nuxt.config.ts

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@vant/nuxt'],
+  script: [
+      { src: 'https://static.line-scdn.net/liff/edge/2.1/sdk.js' }
+    ],
   
 })
